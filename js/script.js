75046e4b19bc185a313f4da656b9f0e38570dfbd
Prodip Udoy <63396894+prodipudoy@users.noreply.github.com>
2021-03-03 21:00:47 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	$('#buttonclick').click(function(){
+		$('.slidemenu').slideToggle(1000)
+	});
+     
+     $('.hide_icon').click(function(){
+		$('.show_content_box').slideToggle(1000)
+	});
+});
